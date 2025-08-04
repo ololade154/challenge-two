26e813fcf4955fbc5c5464dbe08cbe6de6b5eb6a
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-08-04 12:24:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+function App() {
+  return (
+    <div className="App">
+      <Accordion data={faqs}></Accordion>
+    </div>
+  );
+}
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((item, i) => (
+        <AccordionItem
+          title={item.title}
+          text={item.text}
+          num={i}
+          key={item.title}
+        ></AccordionItem>
+      ))}
+    </div>
+  );
+}
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleOpen() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <diV className={`item ${isOpen ? 'open' : ''}`} onClick={handleOpen}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className="content">{text}</div>}
+    </diV>
+  );
+}
+
+export default App;
